e7c930b662e9eddc75c4a4c6ca26810e061131d2
wkyoon <wkyoon77@gmail.com>
2021-04-14 11:36:27 +0900

    chat-example 에 index.html  사용하기

--- app.js
+++ app.js
@@ -1,53 +1,48 @@
-const express = require("express");
-const cors = require('cors')
-const http = require("http");
-const socketIo = require("socket.io");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-
-
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+const port = process.env.PORT || 3001;
+const index = require('./routes/index');
 
 const app = express();
+//app.use(index);
 
-var corsOptions = {
-  origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'http://192.168.0.47:3000',
-  ],
-  methods: ["GET", "POST"],
-  credentials: true,
-};
-
-
-
-
-app.use(index);
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 const server = http.createServer(app);
 
-const io = socketIo(server); 
-io.use(cors(corsOptions));
-let interval;
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+const io = socketIo(server, {
+    cors: {
+        origin: ['http://localhost:3000', 'http://novisfirealert.ga:3000'],
+        methods: ['GET', 'POST'],
+    },
 });
 
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
+let interval;
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+
+    socket.on('chat message', (msg) => {
+        console.log(msg);
+
+        io.emit('chat message', msg);
+    });
+
+    socket.on('fire', (msg) => {
+        console.log(msg);
+        io.emit('fire', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        clearInterval(interval);
+    });
+});
