906ac3873d74324958d1bdc28a61253d2cc5a0f7
wkyoon <wkyoon77@gmail.com>
2021-04-14 11:42:53 +0900

    빠진부분

--- app.js
+++ app.js
@@ -46,3 +46,11 @@ io.on('connection', (socket) => {
         clearInterval(interval);
     });
 });
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
